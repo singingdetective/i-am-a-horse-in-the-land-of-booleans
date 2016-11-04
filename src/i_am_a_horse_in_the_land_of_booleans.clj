c6fb70f337b0f8ffabdaebad124bd5837162bec3
singingdetective <pybus_sean@hotmail.com>
2016-11-05 07:33:28 +1000

    10/05/2016

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -1,28 +1,39 @@
 (ns i-am-a-horse-in-the-land-of-booleans
   (:refer-clojure :exclude [boolean]))
 
-(defn boolean [x]
-  ":(")
+(defn boolean [x] (if x true false))
 
-(defn abs [x]
-  ":(")
+(defn abs [x] (if (< x 0) (* x -1) x))
 
-(defn divides? [divisor n]
-  ":(")
+(defn divides? [divisor n] (if(== 0 (mod n divisor)) true false))
 
-(defn fizzbuzz [n]
-  ":(")
+(defn fizzbuzz [n] (cond
+                      (== 0 (mod n 15)) "gotcha!"
+                      (== 0 (mod n 3))  "fizz"
+                      (== 0 (mod n 5))  "buzz"
+                      :else             ""))
 
 (defn teen? [age]
-  ":(")
+  (< 12 age 20))
 
-(defn not-teen? [age]
-  ":(")
+(defn not-teen? [age] (or (< age 13) (> age 19)))
 
-(defn generic-doublificate [x]
-  ":(")
+(defn generic-doublificate [x] (cond
+                                  (= true (number? x)) (+ x x)
+                                  (and (or (vector? x) (list? x) (map? x)) (== 0 (count x))) nil
+                                  (and (not (map? x)) (< 0 (count x))) (* 2 (count x))
+                                  :else true))
 
 (defn leap-year? [year]
-  ":(")
+  (or
+    (and (== 0 (mod year 4)) (== 0 (mod year 100)) (== 0 (mod year 400)))
+    (and (== 0 (mod year 4)) (not= 0 (mod year 100)))))
+
+(leap-year? 100)
+(leap-year? 200)
+(leap-year? 400)
+(leap-year? 12)
+(leap-year? 20)
+(leap-year? 15)
 
 ; '_______'
